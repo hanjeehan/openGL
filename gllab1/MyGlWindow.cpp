1996e9c842e55016d5ae42587d34062964d88c58
yaozun <julien.wilmet@epitech.eu>
2018-02-02 11:48:16 +0100

    cube viewed, make it turn by uncomentig model lines in MyGlWindow::draw code

--- gllab1/MyGlWindow.cpp
+++ gllab1/MyGlWindow.cpp
@@ -32,9 +32,21 @@ void MyGlWindow::draw(void)
 	glViewport(0, 0, m_width, m_height);
 
     float step = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX / 1000) - 500.0f;
-    step /= 10000;
+    step /= 1000;
     m_model.glPushMatrix();
-    m_model.glTranslate(1 * step, 1 * step, 0);
-    m_model.glRotate(45.0, 0.1, 0.1, 0.1);
-    m_cube->draw(m_model.getMatrix(), m_model.getMatrix(), m_model.getMatrix());
+    //m_model.glTranslate(1 * step, 1 * step, 0);
+    //m_model.glRotate(45.0, 0.1, 0.1, 0.1);
+
+    glm::vec3 eye(5, 5, 5), center(0, 0, 0), up(0, 1, 0);
+    glm::mat4 view = glm::lookAt(eye, center, up);
+
+    glm::mat4 proj = glm::perspective(
+            45.0f,
+            static_cast<float>(m_width) / static_cast<float>(m_height),
+            0.1f,
+            300.0f
+    );
+
+    if (m_cube)
+        m_cube->draw(m_model.getMatrix(), view, proj);
 }
