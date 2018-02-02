fceefbb4e34b88d5c7d71c7880b3729e842518b9
yaozun <julien.wilmet@epitech.eu>
2018-02-02 10:47:47 +0100

    PAPILLON DE LUMIERE rectangle

--- gllab1/MyGlWindow.cpp
+++ gllab1/MyGlWindow.cpp
@@ -33,7 +33,7 @@ void MyGlWindow::setupBuffer()
     shaderProgram->initFromFiles("simple.vert", "simple.frag");
 
     //three vertices
-    const float vertexPosition[] = {
+    /*const float vertexPosition[] = {
             0.0f,  0,    0, 1,
             0,     0.5f, 0, 1,
             0.5f,  0,    0, 1,
@@ -48,6 +48,31 @@ void MyGlWindow::setupBuffer()
             0, 0, 1, //green
             0, 1, 0, //blue
             1, 0, 0  //red
+    };*/
+    GLfloat cube_vertices[] = {
+    // front
+            -1.0, -1.0, 1.0,
+            1.0, -1.0, 1.0,
+            1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.0,
+    // back
+            -1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            1.0, 1.0, -1.0,
+            -1.0, 1.0, -1.0,
+    };
+
+    GLfloat cube_colors[] = {
+    // front colors
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+            1.0, 1.0, 1.0,
+    // back colors
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+            1.0, 1.0, 1.0,
     };
 
     glGenVertexArrays(1, &VAO);
@@ -68,11 +93,11 @@ void MyGlWindow::setupBuffer()
     //bind vbo
     glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices);
     //allocate mem
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4 * 6, &vertexPosition, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * 8, &cube_vertices, GL_STATIC_DRAW);
     //tell gpu how to interpretate the data
     glVertexAttribPointer(
             shaderProgram->attribute("coord3d"), //location of a specific attribute
-            4, // how many data for each vertex
+            3, // how many data for each vertex
             GL_FLOAT, // type
             GL_FALSE,
             0,
@@ -85,7 +110,7 @@ void MyGlWindow::setupBuffer()
     //bind vbo
     glBindBuffer(GL_ARRAY_BUFFER, vbo_color);
     //allocate mem
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * 6, &vertexColors, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * 8, &cube_colors, GL_STATIC_DRAW);
     //tell gpu how to interpretate the data
     glVertexAttribPointer(
             shaderProgram->attribute("v_color"), //location of a specific attribute
@@ -96,6 +121,17 @@ void MyGlWindow::setupBuffer()
             0
     );
     glEnableVertexAttribArray(shaderProgram->attribute("v_color"));
+
+    GLuint ibo_cube_elements;
+    GLushort cube_elements[] = {
+            0, 1, 2,		2, 3, 0,		1, 5, 6,
+            6, 2, 1,		7, 6, 5,		5, 4, 7,
+            4, 0, 3,		3, 7, 4,		4, 5, 1,
+            1, 0, 4,		3, 2, 6,		6, 7, 3,
+    };
+    glGenBuffers(1, &ibo_cube_elements);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_cube_elements);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_elements), cube_elements, GL_STATIC_DRAW);
 
     //close vao
     glBindVertexArray(0);
@@ -120,13 +156,18 @@ void MyGlWindow::draw(void)
     //glUniformMatrix4fv(shaderProgram->uniform("translate"), 1, GL_FALSE, glm::value_ptr(translate));
     //glUniformMatrix4fv(shaderProgram->uniform("rotate"), 1, GL_FALSE, glm::value_ptr(rotate));
 
-    float step = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX / 1000) - 500.0f;
+    /*float step = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX / 1000) - 500.0f;
     step /= 10000;
     m_model.glPushMatrix();
     m_model.glTranslate(1 * step, 1 * step, 0);
     m_model.glRotate(45.0, 0.1, 0.1, 0.1);
-    glUniformMatrix4fv(shaderProgram->uniform("model"), 1, GL_FALSE, glm::value_ptr(m_model.getMatrix()));
+    glUniformMatrix4fv(shaderProgram->uniform("model"), 1, GL_FALSE, glm::value_ptr(m_model.getMatrix()));*/
     //what to draw
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    //VAO
+    //glDrawArrays(GL_TRIANGLES, 0, 6);
+    //with IBO
+    int size;
+    glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE,&size);
+    glDrawElements(GL_TRIANGLES, size / sizeof(GLushort), GL_UNSIGNED_SHORT, 0);
     shaderProgram->disable();
 }
--- simple.vert
+++ simple.vert
@@ -1,10 +1,10 @@
 #version 420 //opengl version 4.0.0
 
-in vec4 coord3d;
+in vec3 coord3d;
 in vec3 v_color;
 
-uniform mat4 translate;
-uniform mat4 rotate;
+//uniform mat4 translate;
+//uniform mat4 rotate;
 
 //uniform mat4 model;
 
@@ -13,7 +13,8 @@ out vec3 color; //all out become in of frag
 void main()
 {
     //gl_Position in built variable that must be set
-    gl_Position = translate * rotate * coord3d;
+    //gl_Position = translate * rotate * coord3d;
     //gl_Position = model * coord3d;
+    gl_Position = coord3d;
     color = v_color;
 }
